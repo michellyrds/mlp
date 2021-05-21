62c4a67ce1ab2ffa772fbd9e9dc9f4105565c502
Michelly <michelly.silva753@gmail.com>
2021-05-21 19:29:21 -0300

    carregar dataset AND no mlp-teste.py

--- mlp.py
+++ mlp.py
@@ -1,82 +1,46 @@
-<<<<<<< HEAD
-"""
-    Implementação do multilayer perceptron
-"""
-
-import numpy as np #importando biblioteca de manipulação de matrizes e etc
-
-
-class MultilayerPerceptron: #declarando a classe do nosso multilayer perceptron
-
-    def __init__(self, dataset):
-        pass
-
-    def fit(self, X, y): # etapa de treino
-        pass
-
-    def predict(self, X): #treinamento de fato
-        pass
-        
-=======
 import numpy as np #importando biblioteca de manipulação de matrizes e etc
 import random
-import funcao_step as fs
 
-class Perceptron:
-    def __init__(self, pesos: np.vectorize, bias):
+class Perceptron: #declarando a classe do nosso neurônio multilayer perceptron
+
+    def __init__(self, pesos, bias):
         self.pesos = pesos
         self.bias = bias
+        pass
 
     def fit(self, X): # etapa de treino
         i = 0
-        for valor in X:
+        for valor in x:
             self.somatoria += self.pesos[i] * valor
             i += 1
-        self.somatoria += self.bias[0]
-        self.somatoria = fs.funcao_step(self.somatoria)
-        #A CONFIRMAR ↓
-        #self.somatoria = fs.d_funcao_step(self.somatoria)
-       
+        self.somatoria += self.bias
+        self.predict(self, self.somatoria)
+        pass
+
+    def predict(self, X): # testes
+        # função de ativação bipolar
+        if (x >= 0):
+            return 1
+        else:
+            return -1
+        pass
+        
 class MultilayerPerceptron: #declarando a classe do nosso  multilayer perceptron
 
     def __init__(self, numPerIni, numPerFim):
         self.numPerIni = numPerIni
         self.numPerFim = numPerFim
-        self.matrizP = []
         pass
 
-    def createPerceptrons(self, qtd_CmE):
-        camada = []
+    def createPerceptrons(self):
         i = 0
-        while i < qtd_CmE:
-            j = 0
-            while j < self.numPerIni:
-                perceptron = Perceptron(self.pesosAleatorios(self.numPerIni), self.pesosAleatorios(1))
-                camada.append(perceptron)
-                # continues...
-                j += 1
-            self.matrizP.append(camada)
-            i += 1
+        while i < self.numPerIni:
+            perceptron = Perceptron(self.pesosAleatorios(self.numPerIni, randon.randrange(-1, 1)))
+            # continues...
+        pass
     
-    def pesosAleatorios(self, numPerIni): #atribui os pesos aleatórios iniciais
-        numAleatorio = []
-        i = 0
-        while i < numPerIni:
-           numAleatorio[i] = random.randrange(-1, 1)
-           i += 1
-        return numAleatorio
+    def pesosAleatorios(numPerIni):
+        for i in numPerIni:
+           numAleatorio[i] = random.randrange(0, 1)
 
-<<<<<<< HEAD
     
->>>>>>> a893382 (28/05)
-=======
-    def epoca(self, instancia):  
-        i = 0
-        while i < len(self.matrizP):
-            j = 0
-            while j < len(self.matrizP):
-                self.matrizP[i][j].fit(instancia)
-                j += 1
-            i += 1
-            
->>>>>>> 5d4600e (alteracoes)
