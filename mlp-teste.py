c83438b11fe9c3a4f602191ea261f9a9eb9d06ed
Michelly <michelly.silva753@gmail.com>
2021-05-21 19:29:21 -0300

    carregar dataset AND no mlp-teste.py

---
+++ mlp-teste.py
@@ -0,0 +1,6 @@
+#carregar os datasets
+import numpy as np
+from numpy import genfromtxt
+
+AND_dataset = np.genfromtxt("datasets/problemAND.csv", delimiter=",", encoding = "UTF-8-sig")
+     
---
+++ mlp.py
@@ -0,0 +1,14 @@
+import numpy as np #importando biblioteca de manipulação de matrizes e etc
+
+
+class MultilayerPerceptron: #declarando a classe do nosso multilayer perceptron
+
+    def __init__(self, dataset):
+        pass
+
+    def fit(self, X, y): # etapa de treino
+        pass
+
+    def predict(self, X): #treinamento de fato
+        pass
+        
