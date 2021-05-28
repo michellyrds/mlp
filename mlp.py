111e3b2600737199e9feafb5899c20fcd8f10a3b
Nanda Cavalcante <49253236+nandacnas@users.noreply.github.com>
2021-05-28 19:14:27 -0300

    28/05

--- mlp.py
+++ mlp.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
     Implementação do multilayer perceptron
 """
@@ -16,3 +17,51 @@ class MultilayerPerceptron: #declarando a classe do nosso multilayer perceptron
     def predict(self, X): #treinamento de fato
         pass
         
+=======
+import numpy as np #importando biblioteca de manipulação de matrizes e etc
+import random
+
+class Perceptron: #declarando a classe do nosso neurônio multilayer perceptron
+
+    def __init__(self, pesos, bias):
+        self.pesos = pesos
+        self.bias = bias
+        pass
+
+    def fit(self, X): # etapa de treino
+        i = 0
+        for valor in x:
+            self.somatoria += self.pesos[i] * valor
+            i += 1
+        self.somatoria += self.bias
+        self.predict(self, self.somatoria)
+        pass
+
+    def predict(self, X): # testes
+        # função de ativação bipolar
+        if (x >= 0):
+            return 1
+        else:
+            return -1
+        pass
+        
+class MultilayerPerceptron: #declarando a classe do nosso  multilayer perceptron
+
+    def __init__(self, numPerIni, numPerFim):
+        self.numPerIni = numPerIni
+        self.numPerFim = numPerFim
+        pass
+
+    def createPerceptrons(self):
+        i = 0
+        while i < self.numPerIni:
+            perceptron = Perceptron(self.pesosAleatorios(self.numPerIni, randon.randrange(-1, 1)))
+            # continues...
+        pass
+    
+    def pesosAleatorios(numPerIni):
+        for i in numPerIni:
+           numAleatorio[i] = random.randrange(0, 1)
+
+    
+>>>>>>> a893382 (28/05)
